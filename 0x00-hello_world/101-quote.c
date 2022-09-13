b52dbc32420be02a4de10a3cd797b91ec16862ea
Mamsana <alicemamsana@gmail.com>
2022-09-12 17:44:32 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - runs the program
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	int i = 2015;
+	int n = 10;
+	int t = 19;
+
+	printf("and that piece of art is useful%c %c", '"', '-');
+	printf(" Dora Korpar, %d%c%d%c%d\n", i, '-', t);
+	return (1);
+}
