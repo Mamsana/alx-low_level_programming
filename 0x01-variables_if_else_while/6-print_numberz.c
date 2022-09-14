86ef0af141241e048242294133ae16a88c6498f0
Mamsana <alicemamsana@gmail.com>
2022-09-14 12:50:53 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'z'; y >= 'a'; y--)
+
+
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char y;
+
+	for (y = 'z'; y >= 'a'; y--)
+
+	{
+		putchar(y);
+	}
+	putchar('\n');
+	return (0);
+}
