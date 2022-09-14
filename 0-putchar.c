b7f3fe80a90acee03992da58c8eb44453f4c455d
Mamsana <alicemamsana@gmail.com>
2022-09-14 14:25:15 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry level
+ *
+ * Description: Prints _putchar
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 0;
+	char c;
+	char s[] = "_putchar";
+
+	while (i <= 7)
+	{
+		c = s[i];
+		putchar(c);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
